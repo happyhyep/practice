d1ddb0f4fe3d741b0269c6116b30cb81f815442c
cobalt70 <giwoo.kim@gmail.com>
2024-05-16 09:39:51 +0900

    Initial Commit

---
+++ Croaks/CroaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CroaksApp.swift
+//  Croaks
+//
+//  Created by Giwoo Kim on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CroaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
